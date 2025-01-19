b31a92d3131d3aea444f7ed251ac87441c3c83c7
Documenter.jl <documenter@juliadocs.github.io>
2025-01-19 08:01:15 +0000

    build based on 924ee03

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FRGDynamicModels","category":"page"},{"location":"#FRGDynamicModels","page":"Home","title":"FRGDynamicModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FRGDynamicModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FRGDynamicModels]","category":"page"}]
+}
